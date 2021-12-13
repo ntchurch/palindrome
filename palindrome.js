acd0c090d8aac551d3b54f4dc292644faa9135db
Nick Church <nicholas.t.church@gmail.com>
2021-12-13 08:59:15 -0700

    Updating scripts

--- palindrome.js
+++ palindrome.js
@@ -4,22 +4,22 @@ function verifyPalindrome(str) {
   if (str != null && str.length > 1) 
   {
     // Convert accent marks
-    var removeAccent = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    var removeAccent = str.normalize("NFD").replace(/[\u0300-\u036f]/g, '');
 
     // Remove spaces
     var collapsedStr = removeAccent.replace(/\s/g, '');
 
-    // Convert special characters
-    var removeSpecial = collapsedStr.replace(/[^a-zA-Z0-9]/g, "")
+    // Remove special characters
+    var removeSpecial = collapsedStr.replace(/[^a-zA-Z0-9]/g, '')
 
     // Switch all letters to lowercase
-    var forwardLower = removeSpecial.toLowerCase();
+    var forwardLowerCase = removeSpecial.toLowerCase();
 
-    // Reverse the string and store for comparison
-    var backwardLower = forwardLower.split('').reverse().join('');
+    // Reverse the string
+    var backwardLowerCase = forwardLowerCase.split('').reverse().join('');
 
     // Compare forward and backward
-    return (backwardLower === forwardLower)
+    return (backwardLowerCase === forwardLowerCase)
   }
   else 
   {
